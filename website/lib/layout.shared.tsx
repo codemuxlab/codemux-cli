9f112ec8853fc261e05c1494163b4e2db26692a2
Ethan Dang <tiencuong.dang@gmail.com>
2025-09-02 15:57:15 +0700

    Add GitHub URL to website navigation

--- website/lib/layout.shared.tsx
+++ website/lib/layout.shared.tsx
@@ -27,5 +27,6 @@ export function baseOptions(): BaseLayoutProps {
     },
     // see https://fumadocs.dev/docs/ui/navigation/links
     links: [],
+    githubUrl: 'https://github.com/codemuxlab/codemux-cli'
   };
 }
